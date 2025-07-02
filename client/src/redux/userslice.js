99d5d5571d7a0ed25abf430871d9f6480f4a2d7d
Abhin prmod <abhinabhi310@gmail.com>
2025-07-02 12:48:55 +0530

    change in user slice to cheak the state

--- client/src/redux/userslice.js
+++ client/src/redux/userslice.js
@@ -3,13 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: null,
 };
-
 const userSlice = createSlice({
+
   name: "user",
   initialState,
   reducers: {
     loginuser: (state, action) => {
       state.user = action.payload;
+        console.log("Payload in loginuser reducer:", action.payload);
+
     },
     logoutuser: (state) => {
       state.user = null;
