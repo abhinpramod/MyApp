19aaa23788e7c2472a7578b4a503a2c86fb1f03c
Abhin prmod <abhinabhi310@gmail.com>
2025-07-31 23:37:32 +0530

    change in redux

--- client/src/redux/userslice.js
+++ client/src/redux/userslice.js
@@ -10,7 +10,6 @@ const userSlice = createSlice({
   reducers: {
     loginuser: (state, action) => {
       state.user = action.payload;
-        console.log("Payload in loginuser reducer:", action.payload);
 
     },
     logoutuser: (state) => {
